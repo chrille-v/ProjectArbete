54081bb58e40e3289c7a92b61ed148ba478f3e0d
Christoffer Vedin <chrillevedin@gmail.com>
2021-11-02 10:27:00 +0100

    Add project files.

---
+++ ProjectArbete/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectArbete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej alla!");
+        }
+    }
+}
