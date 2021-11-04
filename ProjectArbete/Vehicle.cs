d711754daa47afcb8ad198337a23d2da656004ec
Christoffer Vedin <chrillevedin@gmail.com>
2021-11-04 11:02:02 +0100

    Added constructors in Vehicle

--- ProjectArbete/Vehicle.cs
+++ ProjectArbete/Vehicle.cs
@@ -12,5 +12,19 @@ namespace ProjectArbete
         public string Brand { get; set; }
         public string Type { get; set; }
         public int NumberOfTires { get; set; }
+
+        public Vehicle()
+        {
+
+        }
+
+        public Vehicle(int _regnumber, string _color, string _brand, string _type, int _nrTires)
+        {
+            RegNumber = _regnumber;
+            Color = _color;
+            Brand = _brand;
+            Type = _type;
+            NumberOfTires = _nrTires;
+        }
     }
 }
