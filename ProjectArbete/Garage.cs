46bc1d85829a50fdb093efe0bec21a3b470e637c
Christoffer Vedin <chrillevedin@gmail.com>
2021-11-04 10:54:42 +0100

    Added IEnumerable to the Garage class

--- ProjectArbete/Garage.cs
+++ ProjectArbete/Garage.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace ProjectArbete
 {
-    public class Garage
+    public class Garage : IEnumerable
     {
-
+        List<Vehicle> listOfVehicle = new();
+        public IEnumerator GetEnumerator()
+        {
+            return listOfVehicle.GetEnumerator();
+            throw new NotImplementedException();
+        }
     }
 }
