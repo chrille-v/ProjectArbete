40e02da9ec42ee233a8fc6e8a76e2fa86a297488
Christoffer Vedin <chrillevedin@gmail.com>
2021-11-04 11:06:52 +0100

    Added constructors in the car class

--- ProjectArbete/Car.cs
+++ ProjectArbete/Car.cs
@@ -9,5 +9,17 @@ namespace ProjectArbete
     {
         public int NumberOfSeats { get; set; }
         public bool Combi { get; set; }
+
+        public Car()
+        {
+
+        }
+
+        public Car(int _numberOfSeats, bool _combi, int _regnumber, string _color, string _brand, string _type, int _nrTires) 
+            : base(_regnumber, _color, _brand, _type, _nrTires)
+        {
+            NumberOfSeats = _numberOfSeats;
+            Combi = _combi;
+        }
     }
 }
