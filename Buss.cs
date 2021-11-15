9f1fc5d2f7dec881147c3597ad01ada1a6010598
Dawoud Khawari <90610865+Dave0046@users.noreply.github.com>
2021-11-15 11:02:22 +0100

    Add files via upload

---
+++ Buss.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectArbete
+{
+    public class Buss : Vehicle
+    {
+        public bool DubbelDecker { get; set; }
+        public int Seats { get; set; }
+
+        public Buss()
+        {
+
+        }
+
+        public Buss(bool dubbelDecker, int _seats, string _regnumber, string _color, string _brand, string _type, int _nrTires)
+            : base(_regnumber, _color, _brand, _type, _nrTires)
+        {
+            DubbelDecker = dubbelDecker;
+            Seats = _seats;
+        }
+    }
+}
